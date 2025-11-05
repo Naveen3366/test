7e852683e130eee8783e10060708583a217721a5
Naveen3366 <engineernaveen93@gmail.com>
2025-11-05 20:35:45 +0530

    js

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
-const a =10;
-console.log(a);
+
+
+// let a = "name";
+// let b = 1254;
+// let c = true;
+// let d = {key:value}
+// let e = [0,1,2,3,4,5,6]
+// let f = null;
+// let g = undefined;
+
+
+// add();
+
+function add() {
+    a = 20;
+   let b = 30;
+  let  c = a + b;
+    console.log(c)
+}
+
+//  add();
+
+ let book = function(){
+console.log("book")
+ }
+
+//  book();
+
+
+
+//  let a = 9//0
+
